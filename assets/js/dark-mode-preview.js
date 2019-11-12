981ea7cacf4cebf8204e04dade85e4cadcb43a67
zm1990s <zm1990s@gmail.com>
2019-11-12 23:25:39 +0800

    2019111291

--- assets/js/dark-mode-preview.js
+++ assets/js/dark-mode-preview.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
   const cssFile = document.querySelector('[rel="stylesheet"]')
   const originalCssRef = cssFile.getAttribute('href')
   const darkModeCssRef = originalCssRef.replace('just-the-docs.css', 'dark-mode-preview.css')
-  const buttonCopy = ['Return to the light side', 'Preview dark color scheme']
+  const buttonCopy = ['白色主题', '黑色主题']
   const updateButtonText = function(toggleDarkMode) {
     toggleDarkMode.textContent === buttonCopy[0] ?
       toggleDarkMode.textContent = buttonCopy[1] :
