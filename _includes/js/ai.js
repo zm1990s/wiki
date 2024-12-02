01e4a6a4c4dcc6e9f7de592d18a9f97b94029321
matt <zm1990s@gmail.com>
2024-12-02 20:14:50 +0800

    20241202

---
+++ _includes/js/ai.js
@@ -0,0 +1,20 @@
+<script>
+ window.difyChatbotConfig = {
+  token: 'ScLrku0BLipyHrGl',
+  baseUrl: 'http://aisec.halfcoffee.com'
+ }
+</script>
+<script
+ src="http://aisec.halfcoffee.com/embed.min.js"
+ id="ScLrku0BLipyHrGl"
+ defer>
+</script>
+<style>
+  #dify-chatbot-bubble-button {
+    background-color: #1C64F2 !important;
+  }
+  #dify-chatbot-bubble-window {
+    width: 24rem !important;
+    height: 40rem !important;
+  }
+</style>
